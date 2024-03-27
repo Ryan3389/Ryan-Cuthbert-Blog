a80ed2293bcca224dc1db20e0285dfbfb7490b17
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-03-26 23:49:45 -0400

    added comments and fixed href for all links

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,19 +3,23 @@ const titleInput = document.getElementById('title')
 const blogContentInput = document.getElementById('blogContent')
 const submitBtn = document.getElementById('submit-btn')
 
-
+//Array to store user information
 let userInfo = []
+
 submitBtn.addEventListener('click', function (e) {
     saveBlogInfo(e)
 })
 
 function saveBlogInfo(e) {
     e.preventDefault()
+    //checking if any input field is empty
     if (usernameInput.value === '' || titleInput.value === '' || blogContentInput.value === '') {
         alert("Please fill in all input fields")
     } else {
+        //re assigning to userInfo what ever is in local storage, to avoid it resetting after each submission
         userInfo = JSON.parse(localStorage.getItem('userInfo')) || []
 
+        //object containing user data to push into userInfo array
         const blogDataObj = {
             username: usernameInput.value,
             title: titleInput.value,
@@ -24,6 +28,7 @@ function saveBlogInfo(e) {
         userInfo.push(blogDataObj)
         localStorage.setItem('userInfo', JSON.stringify(userInfo))
 
+        //when button is clicked, the user is redirected to blog.html
         window.location.href = 'blog.html'
 
     }
